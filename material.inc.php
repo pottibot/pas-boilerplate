1cac2a8dbe312ca823e3b6f65504a1b104f25e16
roPotsHQ <pietro.l.porcedda@gmail.com>
2022-12-17 16:29:47 +0100

    setting up scss, ts and sftp

--- material.inc.php
+++ material.inc.php
@@ -24,6 +24,8 @@
 
 Example:
 
+// TEST //
+
 $this->card_types = array(
     1 => array( "card_name" => ...,
                 ...
--- modules/js/pasboilerplate.ts
+++ modules/js/pasboilerplate.ts
@@ -1,11 +1,3 @@
-define([
-    "dojo", "dojo/_base/declare",
-    "ebg/core/gamegui",
-    "ebg/counter",
-    "ebg/stock"
-], function (dojo, declare) {
-    return declare("bgagame.yourgamename", ebg.core.gamegui, new Pasboilerplate());
-});
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
@@ -15,111 +7,155 @@ define([
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
  * ----- *
  */
-class Pasboilerplate {
+
+declare const define;
+declare const ebg;
+declare const $;
+declare const dojo: Dojo;
+
+class Pasboilerplate implements Game {
+
+    private gamedatas: YourGameNameGamedatas;
+    private player_id: string;
+    private players: { [playerId: number]: Player };
+    private playerNumber: number;
+
     constructor() {
         console.log('yourgamename constructor');
+            
         // Here, you can init the global variables of your user interface
         // Example:
         // this.myGlobalValue = 0;
+
     }
-    /*
-        setup:
         
-        This method must set up the game user interface according to current game situation specified
-        in parameters.
+        /*
+            setup:
+            
+            This method must set up the game user interface according to current game situation specified
+            in parameters.
+            
+            The method is called each time the game interface is displayed to a player, ie:
+            _ when the game starts
+            _ when a player refreshes the game page (F5)
+            
+            "gamedatas" argument contains all datas retrieved by your "getAllDatas" PHP method.
+        */
+
         
-        The method is called each time the game interface is displayed to a player, ie:
-        _ when the game starts
-        _ when a player refreshes the game page (F5)
+    public setup(gamedatas: any) {
+        console.log( "Starting game setup" );
         
-        "gamedatas" argument contains all datas retrieved by your "getAllDatas" PHP method.
-    */
-    setup(gamedatas) {
-        console.log("Starting game setup");
         // Setting up player boards
-        for (let player_id in gamedatas.players) {
+        for (let player_id in gamedatas.players ) {
             const player = gamedatas.players[player_id];
+                        
             // TODO: Setting up players boards if needed
         }
+        
         // TODO: Set up your game interface here, according to "gamedatas"
+        
+
         // Setup game notifications to handle (see "setupNotifications" method below)
         this.setupNotifications();
-        console.log("Ending game setup");
+
+        console.log( "Ending game setup" );
     }
+    
+
     ///////////////////////////////////////////////////
     //// Game & client states
+    
     // onEnteringState: this method is called each time we are entering into a new game state.
     //                  You can use this method to perform some user interface changes at this moment.
     //
-    onEnteringState(stateName, args) {
-        console.log('Entering state: ' + stateName);
-        switch (stateName) {
-            /* Example:
+    public onEnteringState(stateName: string, args: any) {
+        console.log( 'Entering state: '+stateName );
+        
+        switch( stateName ) {
+        
+        /* Example:
+        
+        case 'myGameState':
+        
+            // Show some HTML block at this game state
+            dojo.style( 'my_html_block_id', 'display', 'block' );
             
-            case 'myGameState':
-            
-                // Show some HTML block at this game state
-                dojo.style( 'my_html_block_id', 'display', 'block' );
-                
-                break;
-            */
-            case 'dummmy':
-                break;
+            break;
+        */
+        
+        
+        case 'dummmy':
+            break;
         }
     }
+
     // onLeavingState: this method is called each time we are leaving a game state.
     //                 You can use this method to perform some user interface changes at this moment.
     //
-    onLeavingState(stateName) {
-        console.log('Leaving state: ' + stateName);
-        switch (stateName) {
-            /* Example:
+    public onLeavingState(stateName: string) {
+        console.log( 'Leaving state: '+stateName );
+        
+        switch( stateName ) {
+        
+        /* Example:
+        
+        case 'myGameState':
+        
+            // Hide the HTML block we are displaying only during this game state
+            dojo.style( 'my_html_block_id', 'display', 'none' );
             
-            case 'myGameState':
-            
-                // Hide the HTML block we are displaying only during this game state
-                dojo.style( 'my_html_block_id', 'display', 'none' );
-                
-                break;
-            */
-            case 'dummmy':
-                break;
-        }
+            break;
+        */
+        
+        
+        case 'dummmy':
+            break;
+        }               
     }
+
     // onUpdateActionButtons: in this method you can manage "action buttons" that are displayed in the
     //                        action status bar (ie: the HTML links in the status bar).
     //        
-    onUpdateActionButtons(stateName, args) {
-        console.log('onUpdateActionButtons: ' + stateName);
-        if (this.isCurrentPlayerActive()) {
-            switch (stateName) {
-                /*
-                                Example:
-                                case 'myGameState':
-                                
-                                // Add 3 action buttons in the action status bar:
-                                
-                                (this as any).addActionButton( 'button_1_id', _('Button 1 label'), 'onMyMethodToCall1' );
-                                (this as any).addActionButton( 'button_2_id', _('Button 2 label'), 'onMyMethodToCall2' );
-                                (this as any).addActionButton( 'button_3_id', _('Button 3 label'), 'onMyMethodToCall3' );
-                                break;
-                */
+    public onUpdateActionButtons(stateName: string, args: any) {
+        console.log( 'onUpdateActionButtons: '+stateName );
+                    
+        if( (this as any).isCurrentPlayerActive() )
+        {            
+            switch( stateName )
+            {
+/*               
+                Example:
+                case 'myGameState':
+                
+                // Add 3 action buttons in the action status bar:
+                
+                (this as any).addActionButton( 'button_1_id', _('Button 1 label'), 'onMyMethodToCall1' ); 
+                (this as any).addActionButton( 'button_2_id', _('Button 2 label'), 'onMyMethodToCall2' ); 
+                (this as any).addActionButton( 'button_3_id', _('Button 3 label'), 'onMyMethodToCall3' ); 
+                break;
+*/
             }
         }
-    }
+}
+
     ///////////////////////////////////////////////////
     //// Utility methods
+    
     /*
     
         Here, you can defines some utility methods that you can use everywhere in your javascript
         script.
     
     */
+
+
     ///////////////////////////////////////////////////
     //// Player's action
+    
     /*
     
-        Here, you are defining methods to handle player's action (ex: results of mouse click on
+        Here, you are defining methods to handle player's action (ex: results of mouse click on 
         game objects).
         
         Most of the time, these methods:
@@ -127,6 +163,7 @@ class Pasboilerplate {
         _ make a call to the game server
     
     */
+    
     /* Example:
     
     onMyMethodToCall1( evt ) {
@@ -137,12 +174,12 @@ class Pasboilerplate {
         // Check that this action is possible (see "possibleactions" in states.inc.php)
         if( ! this.checkAction( 'myAction' ) )
         {   return; }
-        this.ajaxcall( "/yourgamename/yourgamename/myAction.html", {
-                                                                lock: true,
-                                                                myArgument1: arg1,
+        this.ajaxcall( "/yourgamename/yourgamename/myAction.html", { 
+                                                                lock: true, 
+                                                                myArgument1: arg1, 
                                                                 myArgument2: arg2,
                                                                 ...
-                                                                },
+                                                                }, 
                         this, result => {
                         
                         // What to do after the server call if it succeeded
@@ -151,12 +188,15 @@ class Pasboilerplate {
                         }, is_error => {
                         // What to do after the server call in anyway (success or failure)
                         // (most of the time: nothing)
-                        } );
-    },
+                        } );        
+    },        
     
     */
+
+    
     ///////////////////////////////////////////////////
     //// Reaction to cometD notifications
+
     /*
         setupNotifications:
         
@@ -167,15 +207,34 @@ class Pasboilerplate {
     
     */
     setupNotifications() {
-        console.log('notifications subscriptions setup');
+        console.log( 'notifications subscriptions setup' );
+        
         // TODO: here, associate your game notifications with local methods
+        
         // Example 1: standard notification handling
         // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
+        
         // Example 2: standard notification handling + tell the user interface to wait
         //            during 3 seconds after calling the method in order to let the players
         //            see what is happening in the game.
         // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
         // this.notifqueue.setSynchronous( 'cardPlayed', 3000 );
         // 
-    }
+    } 
+    
+    // TODO: from this point and below, you can write your game notifications handling methods
+    
+    /*
+    Example:
+    
+    notif_cardPlayed( notif ) {
+        console.log( 'notif_cardPlayed' );
+        console.log( notif );
+        
+        // Note: notif.args contains the arguments specified during you "notifyAllPlayers" / "notifyPlayer" PHP call
+        
+        // TODO: play the card in the user interface.
+    },    
+    
+    */          
 }
